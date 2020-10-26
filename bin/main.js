7be63672f6c2dbc225ff45b369f851f20b565821
yangxq <yangxq@yjhealth.cn>
2020-10-26 23:07:35 +0800

    移动文档位置

--- bin/main.js
+++ bin/main.js
@@ -10,7 +10,7 @@ const version = require("./../package.json").version;
 program.version(version,"-v,--version") ;  
 
 //创建一个service   
-program.command("add <service-name>")
+program.command("add <service-name>").description('use to create a service')
     .action((name,command)=>{
         const currentDir = process.cwd().split('\\');
         const len = currentDir.length;
@@ -18,13 +18,14 @@ program.command("add <service-name>")
             console.error(chalk.red('切换到packages目录再使用 yj-service-cli add 创建新的模块'))
         }else{
             //判断是否已经存在
-            const isExists = fs.existsSync(path.resolve(process.cwd()+"\\"+name));
+            const servicePath = path.resolve(process.cwd()+"\\"+name);
+            const isExists = fs.existsSync(servicePath);
             if(isExists){
               console.error(chalk.red('该服务已存在'));
             }else{
                 const spinner = ora('开始生成...\n');
                 spinner.start();
-                create(name,spinner,program);
+                create(name,spinner,servicePath);
             }
         }
     })
--- bin/package-add.js
+++ bin/package-add.js
@@ -1,7 +1,8 @@
 const chalk =require('chalk');
 const download = require('download-git-repo');
+const fse = require('fs-extra');
 const url = 'https://github.com:yj-service/yj-service-cli#master';
-module.exports = function(name,spinner,program){
+module.exports = function(name,spinner,servicePath){
     download(url,name,{
         filter:(item)=>{
            return item.type == 'file' && /^template/.test(item.path)
@@ -11,7 +12,14 @@ module.exports = function(name,spinner,program){
             console.error(chalk.red(err)+"\n");
             spinner.stop(); 
         }else{
-            spinner.stop(); 
+            spinner.text = '正在下载模板...'
+            fse.copy(servicePath+'\\template',servicePath,(err)=>{
+                if(err){
+                    console.error(chalk.red('移动失败'))
+                }
+                fse.remove(servicePath+'\\template');
+                spinner.succeed('服务已生成'); 
+            })
         } 
    })
 }
