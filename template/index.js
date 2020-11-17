b3fbd1ef6d7effae5ada7fce9aa291ec4bf5211a
yangxq <yangxq@yjhealth.cn>
2020-11-17 10:11:47 +0800

    提交

--- template/index.js
+++ template/index.js
@@ -3,11 +3,11 @@ import router from './router/index';
 import Index from "./pages/index";
 
 new Vue({
-    router,
-    render: h => h(Index),
+  router,
+  render: h => h(Index),
 }).$mount('#app')
 
-YjService.install = function(Vue){
-  Vue.component(Index.name,Index);
+YjService.install = function (Vue) {
+  Vue.component(Index.name, Index);
 }
 export default YjService;
