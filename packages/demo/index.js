67054efe28e041ae4fb520a777d3ea96023a4fb0
yangxq <yangxq@yjhealth.cn>
2020-11-19 22:20:31 +0800

    添加demo

---
+++ packages/demo/index.js
@@ -0,0 +1,13 @@
+import Vue from 'vue';
+import router from './router/index';
+import Index from "./pages/index";
+
+new Vue({
+  router,
+  render: h => h(Index),
+}).$mount('#app')
+
+YjService.install = function (Vue) {
+  Vue.component(Index.name, Index);
+}
+export default YjService;
