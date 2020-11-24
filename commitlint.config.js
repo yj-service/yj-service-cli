bad48abfab8409cdf81e5bfc18b93a79ad104375
yangxq <yangxq@yjhealth.cn>
2020-11-24 13:27:14 +0800

    提交

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,19 +1,19 @@
 module.exports = {
     extends: ['@commitlint/config-conventional'],
-    rules: {
-        'type-enum': [2, 'always', [
-            'build',    //构建提交 
-            'ci',       //持续集成
-            'chore',    //其他类型 
-            'docs',     //文档更新
-            'feat',     //新增功能
-            'fix',      //修复问题
-            'perf',     //性能优化 
-            'refactor', //代码重构
-            'revert',   //回滚提交
-            'style',    //修改样式
-            'test'      //测试相关  
-        ]]
-    }
+    // rules: {
+    //     'type-enum': [2, 'always', [
+    //         'build',    //构建提交 
+    //         'ci',       //持续集成
+    //         'chore',    //其他类型 
+    //         'docs',     //文档更新
+    //         'feat',     //新增功能
+    //         'fix',      //修复问题
+    //         'perf',     //性能优化 
+    //         'refactor', //代码重构
+    //         'revert',   //回滚提交
+    //         'style',    //修改样式
+    //         'test'      //测试相关  
+    //     ]]
+    // }
 
 }
