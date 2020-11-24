b6c448510f786f102894ad26d8e5436f26c80d4e
yangxq <yangxq@yjhealth.cn>
2020-11-24 15:27:59 +0800

    feat: 提交代码

--- bin/package-add.js
+++ bin/package-add.js
@@ -10,6 +10,7 @@ const {downloadTpl} = require('./util');
 const {gitRepository} = require('./config');
 
 module.exports = function createPackage (name,argv){
+    
     fse.ensureDirSync(process.cwd()+"\\packages");  //确保packages存在
     //判断服务是否已经存在
     const servicePath = path.resolve(process.cwd()+"\\packages\\"+name);
