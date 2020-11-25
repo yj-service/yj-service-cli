9b5e35195a0333cd3f8be09200b8405c4fc9a69d
yangxq <yangxq@yjhealth.cn>
2020-11-25 13:20:55 +0800

    feat: 添加别名

--- template-init/vue.config.js
+++ template-init/vue.config.js
@@ -1,10 +1,12 @@
 const glob = require("glob");
+const { resolve } = require("path");
 const path = require("path");
 
 const buildModule = process.argv.splice(3);
 const isBuildModule = buildModule.length>0;
 function getEneries() {
     let entries = {};
+    let alias = {};
     const paths = glob.sync("./packages/*/index.js");
     for (let entry of paths) {
       const moduleName = path.basename(path.dirname(entry));
@@ -15,6 +17,7 @@ function getEneries() {
         template:"public/index.html",
         filename:filename
       };
+      alias[`@${moduleName}`] = path.resolve(`package\\${moduleName}`)
     }
     return entries;
 }
@@ -23,5 +26,15 @@ module.exports = {
     publicPath:"./",
     productionSourceMap:process.env.NODE_ENV == 'production'?false:true,
     assetsDir:"static",
-    pages:isBuildModule ? {[buildModule]:pages[buildModule]} : pages
+    pages:isBuildModule ? {[buildModule]:pages[buildModule]} : pages,
+    configureWebpack:{
+      resolve: {
+        alias: alias
+      },
+      output: {
+        library: `${name}-[name]`,
+        libraryTarget: 'umd',
+        jsonpFunction: `webpackJsonp_${name}`,
+      },
+    }
 }
